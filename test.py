fb0515f603d8c88fea0e35cdcd5aa9465a970890
gziz <69287011+gziz@users.noreply.github.com>
2023-02-13 13:31:16 -0600

    Added audio file

--- test.py
+++ test.py
@@ -9,6 +9,7 @@ from plots import plot_spectrogram
 
 
 torch_data, sr  = torchaudio.load('lex_6.wav')
+# Convert audio array to mel spectrogram
 librosa_mel = librosa.feature.melspectrogram(y=torch_data[0].numpy(),
                                              n_fft=2048,
                                              win_length=1024,
@@ -26,20 +27,9 @@ sAug = SpecAugment(
             time_mask_p = 1.0,
 )
 augmented = sAug(librosa_mel)
+
+# Plot only receives 2D
 if augmented.dim() == 3:
     augmented = augmented[0].squeeze()
-
-### Fairseq ###
-# fairseq_spec = \
-#     SpecAugmentTransform(
-#         time_warp_w = 150,
-#         freq_mask_n = 2,
-#         freq_mask_f = 40,
-#         time_mask_n = 3,
-#         time_mask_t = 10,
-#         time_mask_p = 1.0,
-# )
-# augmented = fairseq_spec(librosa_mel)
-
 
 plot_spectrogram(augmented)
