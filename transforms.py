67d2783e80ceff5b07b068368ce9f43c38836da6
gziz <69287011+gziz@users.noreply.github.com>
2023-02-23 19:54:19 -0600

    Added Docs

--- transforms.py
+++ transforms.py
@@ -39,8 +39,8 @@ class SpecAugment(torch.nn.Module):
         self.mask_value = mask_value
 
         
-    def forward(self, specgram):
-
+    def forward(self, specgram: torch.Tensor):
+        "Apply SpecAugment to spectrogram with dimensions (batch, frequency, time)"
         if specgram.dim() == 2:
             specgram = specgram.unsqueeze_(0)
 
