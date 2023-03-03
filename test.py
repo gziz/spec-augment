8ffeda7f3c52c6efef61c7221ad07a57c26cd4d7
gziz <69287011+gziz@users.noreply.github.com>
2023-03-03 09:30:08 -0600

    add args parse

--- test.py
+++ test.py
@@ -1,38 +1,36 @@
-import torch
-import torchaudio
-import librosa
+import argparse
 
 from transforms import SpecAugment
-from functional import warp_axis_torch, mask_along_axis
-from utils.plots import plot_spectrogram
+from functional import *
+from utils.spectrogram import plot_specgram, create_specgram
 
 
-def create_specgram(audio_path: str, num_batch: int):
-    torch_data, sr  = torchaudio.load(audio_path)
-    # Convert audio array to mel spectrogram
-    librosa_mel = librosa.feature.melspectrogram(y=torch_data[0].numpy(),
-                                                n_fft=2048,
-                                                win_length=1024,
-                                                sr=sr)
+batch_specgram = create_specgram("audio_data/lex_30.wav", batch_sz = 100)
 
-    tensor_mel = torch.tensor(librosa_mel)
-    stacked_mel = tensor_mel.repeat((num_batch,1,1))
-    return stacked_mel
+parser = argparse.ArgumentParser(add_help=False)
+parser.add_argument("--paradigm", type=str, default="modular")
+parser.add_argument("--loops", type=int, default=1)
+args = parser.parse_args()
+
+params = {
+    "warp_axis": 2,
+    "warp_param": 1000,
+    "freq_mask_num": 3,
+    "freq_mask_param": 30,
+    "freq_mask_p": 0.1,
+    "time_mask_num": 3,
+    "time_mask_param": 500,
+    "time_mask_p": 0.5
+}
 
 
+if args.paradigm == "modular":
+    spec = SpecAugment(**params)
+    for _ in range(args.loops):
+        augmented = spec(batch_specgram)
 
-batch_specgram = create_specgram("audio_data/lex_30.wav", 100)
+else:
+    for _ in range(args.loops):
+        augmented = spec_augment(batch_specgram, **params)
 
-spec = SpecAugment(
-        warp_axis = 2,
-        warp_w = 500,
-        freq_mask_num = 3,
-        freq_mask_param = 0,
-        freq_mask_p = 0.1,
-        time_mask_num = 3,
-        time_mask_param = 0,
-        time_mask_p = .05
-)
-for _ in range(500):
-    augmented = spec(batch_specgram)
-#plot_spectrogram(augmented[0])
+plot_specgram(augmented[0])
